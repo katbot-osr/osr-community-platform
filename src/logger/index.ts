21c69e26dc1a29c25360f67473483dd118127925
Luke Watts <luke@thisis.la>
2023-04-29 09:41:04 +0200

    feat: adopt tslog

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,32 +1,46 @@
-import Logger from 'pino'
-import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
 import { getConfigurationOption } from '../config/config'
+import { Logger } from 'tslog'
+import { LogflareHttpClient } from 'logflare-transport-core'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const logLevel = getConfigurationOption('REACT_APP_LOG_LEVEL', 'info')
 
-let loggerInstance: Logger.Logger
-if (getConfigurationOption('REACT_APP_LOGFLARE_KEY', '')) {
-  const logflareConfiguration = {
-    apiKey: getConfigurationOption('REACT_APP_LOGFLARE_KEY', ''),
-    sourceToken: getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', ''),
-  }
-
-  loggerInstance = Logger(
-    {
-      browser: {
-        transmit: {
-          send: createPinoBrowserSend(logflareConfiguration),
-        },
-      },
-      level: logLevel,
-    },
-    createWriteStream(logflareConfiguration),
-  )
-} else {
-  loggerInstance = Logger({
-    browser: { asObject: false },
-    level: logLevel,
-  })
+const levelNumberToNameMap = {
+  silly: 0,
+  trace: 1,
+  debug: 2,
+  info: 3,
+  warn: 4,
+  error: 5,
+  fatal: 6,
 }
 
-export const logger = loggerInstance
+export const logger = new Logger({
+  type: window.location.hostname === 'localhost' ? 'pretty' : 'hidden',
+  minLevel: levelNumberToNameMap[logLevel],
+  hideLogPositionForProduction: true,
+})
+
+const logFlareTransport = (props) => {
+  try {
+    const client = new LogflareHttpClient({
+      apiKey: props.LOGFLARE_KEY,
+      sourceToken: props.LOGFLARE_SOURCE,
+    })
+    return (logObject: any) => {
+      client.addLogEvent({
+        message: logObject[0],
+        metadata: logObject[1],
+      })
+    }
+  } catch (err) {
+    return () => { }
+  }
+}
+
+logger.attachTransport(
+  logFlareTransport({
+    LOGFLARE_KEY: getConfigurationOption('REACT_APP_LOGFLARE_KEY', ''),
+    LOGFLARE_SOURCE: getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', ''),
+  }),
+)
